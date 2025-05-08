caa758932b1e7cbc3a3bfeb9dfbced5863d881b2
brndn6543 <brndn.6543@gmail.com>
2025-05-08 09:48:24 -0400

    Added function pointer demo.

---
+++ include/function_pointers_demo.h
@@ -0,0 +1,13 @@
+//
+// Created by wrvth on 5/8/2025.
+//
+
+#ifndef FUNCTION_POINTERS_DEMO_H
+#define FUNCTION_POINTERS_DEMO_H
+
+int add(int a, int b);
+
+// A global function-pointer declaration:
+// extern int (*fnPtr)(int, int);
+
+#endif //FUNCTION_POINTERS_DEMO_H
