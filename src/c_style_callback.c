58252c4cf1f3d0ee691a119b7d5c68d7b578fa1c
brndn6543 <brndn.6543@gmail.com>
2025-05-08 15:17:27 -0400

    Updated comments.

--- src/c_style_callback.c
+++ src/c_style_callback.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 #include "c_style_callback.h"
+/* Using callbacks in this way allows you to bundle data and
+ * behavior, essentially encapsulation.
+ *
+ *     State (.context)
+ *     Behavior (.onClick)
+ *
+ * The only way to interact with that state is through the functions
+ * you expose and whatever callbacks you wire up.
+ *
+ * By swapping out different onClick functions while keeping the
+ * same struct shape, you get polymorphic behavior in C.
+ */
 
 /* Define the callback type:
  *     Takes a void* context (user data) and returns nothing.
